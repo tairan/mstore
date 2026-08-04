6c8272b0208291d0c400fa8b7617844464b01089
WANG TAIRAN <tairan.wang@gmail.com>
2026-08-04 13:45:47 +0800

    Bump version to 0.3.4

--- cmd/mstore/main.go
+++ cmd/mstore/main.go
@@ -21,7 +21,7 @@ import (
 	"github.com/chieworks/mstore/internal/store"
 )
 
-var version = "0.3.3"
+var version = "0.3.4"
 
 type globalOptions struct {
 	store   string
--- cmd/mstore/main_test.go
+++ cmd/mstore/main_test.go
@@ -152,7 +152,7 @@ func TestCLIHelpIsAlignedAndIncludesGenerate(t *testing.T) {
 
 func TestCLIVersion(t *testing.T) {
 	var out, errOut bytes.Buffer
-	if code := run([]string{"--version"}, &out, &errOut); code != 0 || out.String() != "mstore 0.3.3\n" {
+	if code := run([]string{"--version"}, &out, &errOut); code != 0 || out.String() != "mstore 0.3.4\n" {
 		t.Fatalf("code=%d stdout=%q stderr=%q", code, out.String(), errOut.String())
 	}
 }
